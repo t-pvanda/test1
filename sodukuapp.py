4541c86b32f6bf9aa9afff3f8ac8187464a8f443
t-pvanda <41090366+t-pvanda@users.noreply.github.com>
2019-08-24 00:54:05 -0700

    Add files via upload

--- sodukuapp.py
+++ sodukuapp.py
@@ -1,9 +1,151 @@
-import csv
-import array
+import numpy as np
 
-with open ('soduku.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+zero_arr = np.zeros (9, dtype="uint8")        # used to turn bool to 1 for true
+one_arr = np.ones (9, dtype="uint8")        # used to turn bool to 1 for true
+solving = np.zeros([9,9,9], dtype="uint8")    # each row is possible numbers in a square - L to R, top to bottom
+filled_cell = np.zeros([9,9], dtype="uint8")
 
-    for line in csv_reader:
-        print(line)
+puzzle = np.loadtxt('soduku_easy.csv', dtype="uint8", delimiter=",")
+print(puzzle)
+# tmp = puzzle[0] == 3
+# solv_3 = tmp+zero_arr
+x=0
+while x <= 8: # loop to move through lines in the puzzle array and create solving and filled_cell arrays
+    y=0
+    while y <= 8: # loop to test for each number
+        solving[x, y, :] = zero_arr
+        z=y+1
+        tmp = puzzle[x] == z
+        # print (tmp)
+        if True in tmp:
+            is_it = one_arr # one array marks this number as not existing in the cell
+        else:
+            is_it = zero_arr
+        # tmp = zero_arr + tmp
+        # print(x, z, tmp)
+        # print(x, z, is_it)
 
+        solving[x, y, :] = (solving[x, y, :] + is_it)
+        filled_cell[x, :] = (filled_cell[x, :] + tmp)
+        y += 1
+    x += 1
+# print (solving)
+# print (solving[0,:,0])
+# print (filled_cell)
+# print (filled_cell[:,0])
+x=0
+while x <= 8: # checking column matches
+    y=0
+    while y <= 8: # loop to check each cell
+        z=0
+        while z <= 8: # loop to check each number in the column and mark it out
+            number = z+1
+            # print (filled_cell[z,y])
+            if puzzle[z,y] > 0:
+                tmp = puzzle[z,y]
+                tmp = tmp-1
+                # print (x,tmp,y)
+                solving[x,tmp,y] = 1
+                # print (solving[z,y,tmp])
+            z += 1
+        y += 1
+    x += 1
+# print (solving)
+
+# this loop is solving for rows and columns
+x=0
+while x <= 8: # loop to find and mark solved numbers in the puzzle
+    y=0
+    while y <= 8:
+        # print(puzzle[x,y])
+        if puzzle[x,y] == 0:
+            # print(solving[x, :, y])
+            tmp = np.sum(solving[x, :, y])
+
+            if tmp == 8:
+                dam = solving[x,:,y]
+                damit = np.ndarray.tolist(dam)
+                dammitt = damit.index(0)+1
+                # print(dammitt)
+                puzzle[x,y] = (dammitt)
+                # print(x,y,puzzle[x,y])
+                # print(puzzle)
+        y += 1
+    x += 1
+
+# print(puzzle)
+
+# this loop is solving for each sub cell of 9
+for index in [0,3,6]:
+    z = 0
+    tmp = 0
+    # if x == 8 and y << 8:
+    #     x = 0
+    x = 0+index
+    while x <= 2+index:  # loop to find and mark solved numbers in the puzzle
+        y = 0+index
+        while y <= 2+index:
+            # print(((x+1) % 3), ((y+1) % 3))
+            print (x, y)
+            tmp = tmp+filled_cell[x, y]
+            # if puzzle[x, y] == 0:
+            #     # print(solving[x, :, y])
+            #     tmp = np.sum(solving[x, :, y])
+            #
+            #     if tmp == 8:
+            #         dam = solving[x, :, y]
+            #         damit = np.ndarray.tolist(dam)
+            #         dammitt = damit.index(0) + 1
+            #         # print(dammitt)
+            #         puzzle[x, y] = (dammitt)
+            #         # print(x,y,puzzle[x,y])
+
+            if (not ((x + 1) % 3) and (not (y + 1) % 3)):
+                print("hi", ((x + 1) % 3), ((y + 1) % 3))
+                z = 0
+            y += 1
+        x += 1
+
+# print (puzzle)
+# print (solving)
+# print (np.size(solving))
+# print(filled_cell)
+
+# # print(puzzle[0:0, 2:2])
+# print(puzzle[0])
+# print(puzzle[:,0])
+# print(puzzle[0] == 3)
+
+
+
+# ------ second try ------
+# columns = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# dict = {}
+# col_out = []
+# x=0
+# while x <= 8:
+#     key=("col" + str(x))
+#     # name = "col" + str(x)
+#     name = puzzle[[0, 1, 2, 3, 4, 5, 6, 7, 8], [x, x, x, x, x, x, x, x, x]]
+#     out = [key, name]
+#     col_out = col_out + out
+#     x += 1
+# # print (out[0], out[1])
+# print (col_out)
+
+# ------ first try ------
+# rows = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8], dtype=np.uint)
+# cols = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8], dtype=np.uint)
+# print(puzzle[rows[:, np.newaxis], cols])
+
+# actual_col1 = np.array([[0,0], [1,0], [2,0], [3,0], [4,0], [5,0], [6,0], [7,0], [8,0]], dtype=np.uint)
+# actual_col1 = np.array([[0,0], [1,0], [2,0]], dtype=np.uint)
+# print(puzzle[actual_col1])
+
+
+# x = np.array([[[1,9,8],[2,7,6],[3,5,4]], [[4,10,11],[5,12,13],[6,14,15]]])
+# x = np.array([[1,9,8],[2,7,6],[3,5,4]])
+# print(x[[0, 1, 2], [0, 0, 0]])
+# rows = np.array([[0, 0], [1, 1], [2, 2]], dtype=np.uint)
+# cols = np.array([[0, 0], [1, 1], [2, 2]], dtype=np.uint)
+# print(x[rows])
--- testapp.py
+++ testapp.py
@@ -6,19 +6,17 @@ solving = np.zeros([9,9,9], dtype="uint8")    # each row is possible numbers in 
 filled_cell = np.zeros([9,9], dtype="uint8")
 
 puzzle = np.loadtxt('soduku_easy.csv', dtype="uint8", delimiter=",")
-# print(puzzle)
-# tmp = puzzle[0] == 3
-# solv_3 = tmp+zero_arr
+
 x=0
 while x <= 8: # loop to move through lines in the puzzle array and create solving and filled_cell arrays
     y=0
     while y <= 8: # loop to test for each number
         solving[x, y, :] = zero_arr
-        z=y+1
+        z = y + 1
         tmp = puzzle[x] == z
         # print (tmp)
         if True in tmp:
-            is_it = one_arr
+            is_it = one_arr # one array marks this number as not existing in the cell
         else:
             is_it = zero_arr
         # tmp = zero_arr + tmp
@@ -27,12 +25,14 @@ while x <= 8: # loop to move through lines in the puzzle array and create solvin
 
         solving[x, y, :] = (solving[x, y, :] + is_it)
         filled_cell[x, :] = (filled_cell[x, :] + tmp)
+        # print (puzzle[x,y])
+        # print (filled_cell[x, :])
         y += 1
     x += 1
 # print (solving)
 # print (solving[0,:,0])
-# print (filled_cell)
 # print (filled_cell[:,0])
+
 x=0
 while x <= 8: # checking column matches
     y=0
@@ -50,91 +50,70 @@ while x <= 8: # checking column matches
             z += 1
         y += 1
     x += 1
+
 # print (solving)
 
-# this loop is solving for rows and columns
-x=0
-while x <= 8: # loop to find and mark solved numbers in the puzzle
-    y=0
-    while y <= 8:
-        # print(puzzle[x,y])
-        if puzzle[x,y] == 0:
-            # print(solving[x, :, y])
-            tmp = np.sum(solving[x, :, y])
+def line_func (puzzle):
+    # this loop is solving for rows and columns
+    x=0
+    while x <= 8: # loop to find and mark solved numbers in the puzzle
+        y=0
+        while y <= 8:
+            # print(puzzle[x,y])
+            if puzzle[x,y] == 0:
+                # print(solving[x, :, y])
+                tmp = np.sum(solving[x, :, y])
 
-            if tmp == 8:
-                dam = solving[x,:,y]
-                damit = np.ndarray.tolist(dam)
-                dammitt = damit.index(0)+1
-                # print(dammitt)
-                puzzle[x,y] = (dammitt)
-                # print(x,y,puzzle[x,y])
-                # print(puzzle)
-        y += 1
-    x += 1
+                if tmp == 8:
+                    dam = solving[x,:,y]
+                    damit = np.ndarray.tolist(dam)
+                    dammitt = damit.index(0)+1
+                    # print(dammitt)
+                    puzzle[x,y] = (dammitt)
+                    filled_cell[x, y] = 1
+                    # print(x,y,puzzle[x,y])
+                    # print(puzzle)
+            y += 1
+        x += 1
+    return (puzzle)
 
-print(puzzle)
+print (puzzle)
+# print (filled_cell)
 
-# this loop is solving for each sub cell of 9
-x = 0
-while x <= 8:  # loop to find and mark solved numbers in the puzzle
-    y = 0
-    while y <= 8:
-        # print(puzzle[x,y])
-        if puzzle[x, y] == 0:
-            # print(solving[x, :, y])
-            tmp = np.sum(solving[x, :, y])
+def block_func (puzzle):
+    # this loop is solving for each sub cell of 9
+    for index in [0,3,6]:
+        subcellsum = 0
+        tmp = 0
+        x=0
+        while x <= 8:
+            y= 0+index
+            while y <= 2+index: # loop to check each subcell of 9
+                tmp = tmp + filled_cell[x, y]
+                subcellsum = (subcellsum + (puzzle[x, y]))
+                # print (subcellsum, x, y)
+                if filled_cell[x, y] == 0:
+                    emptyx = x
+                    emptyy = y
+                y += 1
+            x += 1
+            if not (x % 3):
+                if tmp == 8:
+                    # print(emptyx, emptyy, subcellsum)
+                    cellfillvalue = 45 - subcellsum
+                    puzzle[emptyx, emptyy] = cellfillvalue
+                    filled_cell[emptyx, emptyy] = 1
+                subcellsum = 0
+                tmp = 0
+    return (puzzle)
 
-            if tmp == 8:
-                dam = solving[x, :, y]
-                damit = np.ndarray.tolist(dam)
-                dammitt = damit.index(0) + 1
-                # print(dammitt)
-                puzzle[x, y] = (dammitt)
-                # print(x,y,puzzle[x,y])
-                # print(puzzle)
-        y += 1
-    x += 1
+line_func(puzzle)
+block_func(puzzle)
+line_func(puzzle)
+block_func(puzzle)
 
+# print(filled_cell)
+print (puzzle)
 # print (solving)
 # print (np.size(solving))
 # print(filled_cell)
-
-# # print(puzzle[0:0, 2:2])
-# print(puzzle[0])
-# print(puzzle[:,0])
-# print(puzzle[0] == 3)
-
-
-
-# ------ second try ------
-# columns = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-# dict = {}
-# col_out = []
-# x=0
-# while x <= 8:
-#     key=("col" + str(x))
-#     # name = "col" + str(x)
-#     name = puzzle[[0, 1, 2, 3, 4, 5, 6, 7, 8], [x, x, x, x, x, x, x, x, x]]
-#     out = [key, name]
-#     col_out = col_out + out
-#     x += 1
-# # print (out[0], out[1])
-# print (col_out)
-
-# ------ first try ------
-# rows = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8], dtype=np.uint)
-# cols = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8], dtype=np.uint)
-# print(puzzle[rows[:, np.newaxis], cols])
-
-# actual_col1 = np.array([[0,0], [1,0], [2,0], [3,0], [4,0], [5,0], [6,0], [7,0], [8,0]], dtype=np.uint)
-# actual_col1 = np.array([[0,0], [1,0], [2,0]], dtype=np.uint)
-# print(puzzle[actual_col1])
-
-
-# x = np.array([[[1,9,8],[2,7,6],[3,5,4]], [[4,10,11],[5,12,13],[6,14,15]]])
-# x = np.array([[1,9,8],[2,7,6],[3,5,4]])
-# print(x[[0, 1, 2], [0, 0, 0]])
-# rows = np.array([[0, 0], [1, 1], [2, 2]], dtype=np.uint)
-# cols = np.array([[0, 0], [1, 1], [2, 2]], dtype=np.uint)
-# print(x[rows])
